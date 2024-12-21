90272f0b6f69358c0397d49ac753f83793a37917
itzradhika99 <lalithakalyani89@gmail.com>
2024-12-21 20:39:11 +0530

    Create LandingPage.js

---
+++ Frontend/src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function LandingPage() {
+  return (
+    <div>
+      <h1>Welcome to PayMate</h1>
+      <p>Your trusted digital payment wallet.</p>
+    </div>
+  );
+}
+
+export default LandingPage;
